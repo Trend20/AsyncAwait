d0c760f4b5dfa536dfdd7b03c9228df83081f191
Trend20 <enockomondi305@gmail.com>
2020-06-14 09:14:02 -0400

    callback

--- callback.js
+++ callback.js
@@ -8,6 +8,16 @@ function getPosts(){
         let output = '';
         posts.forEach((post,index) =>{
             output += `<li>${post.title}</li>`;
-        })
-    })
+        });
+        document.body.innerHTML = output;
+    }, 1000);
 }
+
+function createPost(post,callback){
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+
+createPost({ title: 'Post Three', body: ' This is Post Three'},getPosts);
